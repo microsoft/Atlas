c06b5af55cec1f6735104b64c0468cfc771208e6
Louis DeJardin <lodejard@microsoft.com>
2018-09-06 15:51:47 -0700

    Adding dotnet test to build-ci.yaml and build.cmd

--- build.cmd
+++ build.cmd
@@ -3,6 +3,8 @@ SET BinFolder=%~dp0bin
 
 dotnet restore src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj
 dotnet build src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release
+dotnet test test/Microsoft.Atlas.CommandLine.Tests/Microsoft.Atlas.CommandLine.Tests.csproj -c Release
+
 dotnet publish src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release -o %BinFolder%\atlas
 dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateTarball /p:RuntimeIdentifier=linux-x64 /p:TargetFramework=netcoreapp2.0 /p:Configuration=Release /p:ArchiveDir=%BinFolder%\downloads
 dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateZip /p:RuntimeIdentifier=win10-x64 /p:TargetFramework=netcoreapp2.0 /p:Configuration=Release /p:ArchiveDir=%BinFolder%\downloads
