ba0aa94a1aa23cb8df4c05dff036b80118c88195
Louis DeJardin <lodejard@microsoft.com>
2018-09-24 14:19:14 -0700

    Updating various build scripts to 2.1

--- build.cmd
+++ build.cmd
@@ -5,10 +5,7 @@ SET BinFolder=%~dp0bin
 dotnet restore --force
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet build src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-dotnet build test/Microsoft.Atlas.CommandLine.Tests/Microsoft.Atlas.CommandLine.Tests.csproj -c Release
+dotnet build -c Release
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet test test/Microsoft.Atlas.CommandLine.Tests/Microsoft.Atlas.CommandLine.Tests.csproj -c Release
@@ -17,7 +14,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet publish src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release -o %BinFolder%\atlas
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet pack src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release -o %BinFolder%\tool
+dotnet pack src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release -o %BinFolder%\tools
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateTarball /p:RuntimeIdentifier=linux-x64 /p:TargetFramework=netcoreapp2.1 /p:Configuration=Release /p:ArchiveDir=%BinFolder%\downloads
