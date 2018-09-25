7d35662e05c8d12fa91422e78ddadfb2813f03eb
Louis DeJardin <lodejard@microsoft.com>
2018-09-25 11:27:29 -0700

    Pack as global tool (#21)
    * Updating for dotnet 2.1
    
    * Packing console app as dotnet tool
    
    * Moving metadata in Directory.Build.props to apply to all packages
    
    * Including license and notice files in dotnet tool package
    
    * Pushing global tool nupkg to static-file NuGet feed
    
    * Updating installation instructions to include global tool

--- build.cmd
+++ build.cmd
@@ -2,13 +2,10 @@ SETLOCAL
 CD %~dp0
 SET BinFolder=%~dp0bin
 
-dotnet restore src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj
+dotnet restore --force
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet build src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-dotnet build test/Microsoft.Atlas.CommandLine.Tests/Microsoft.Atlas.CommandLine.Tests.csproj -c Release
+dotnet build -c Release
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet test test/Microsoft.Atlas.CommandLine.Tests/Microsoft.Atlas.CommandLine.Tests.csproj -c Release
@@ -17,13 +14,16 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet publish src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release -o %BinFolder%\atlas
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateTarball /p:RuntimeIdentifier=linux-x64 /p:TargetFramework=netcoreapp2.0 /p:Configuration=Release /p:ArchiveDir=%BinFolder%\downloads
+dotnet pack src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release -o %BinFolder%\tools
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateZip /p:RuntimeIdentifier=win10-x64 /p:TargetFramework=netcoreapp2.0 /p:Configuration=Release /p:ArchiveDir=%BinFolder%\downloads
+dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateTarball /p:RuntimeIdentifier=linux-x64 /p:TargetFramework=netcoreapp2.1 /p:Configuration=Release /p:ArchiveDir=%BinFolder%\downloads
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet pack src/Microsoft.Atlas.CommandLine.Chocolatey/Microsoft.Atlas.CommandLine.Chocolatey.csproj -o %BinFolder%\chocolatey
+dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateZip /p:RuntimeIdentifier=win10-x64 /p:TargetFramework=netcoreapp2.1 /p:Configuration=Release /p:ArchiveDir=%BinFolder%\downloads
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+dotnet pack src/Microsoft.Atlas.CommandLine.Chocolatey/Microsoft.Atlas.CommandLine.Chocolatey.csproj -c Release -o %BinFolder%\chocolatey
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 call src/Tasks/build.cmd
--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BinFolder="${DIR}/bin"
+set -e
 
-dotnet restore src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj
-dotnet build src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release
+dotnet restore --force
+dotnet build -c Release
 dotnet test test/Microsoft.Atlas.CommandLine.Tests/Microsoft.Atlas.CommandLine.Tests.csproj -c Release
 
 dotnet publish src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release -o ${BinFolder}/atlas
-dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateTarball /p:RuntimeIdentifier=linux-x64 /p:TargetFramework=netcoreapp2.0 /p:Configuration=Release /p:ArchiveDir=${BinFolder}/downloads
-dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateZip /p:RuntimeIdentifier=win10-x64 /p:TargetFramework=netcoreapp2.0 /p:Configuration=Release /p:ArchiveDir=${BinFolder}/downloads
-dotnet pack src/Microsoft.Atlas.CommandLine.Chocolatey/Microsoft.Atlas.CommandLine.Chocolatey.csproj -o ${BinFolder}/chocolatey
+dotnet pack src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj -c Release -o ${BinFolder}/tools
+dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateTarball /p:RuntimeIdentifier=linux-x64 /p:TargetFramework=netcoreapp2.1 /p:Configuration=Release /p:ArchiveDir=${BinFolder}/downloads
+dotnet msbuild src/Microsoft.Atlas.CommandLine/Microsoft.Atlas.CommandLine.csproj /t:Restore,CreateZip /p:RuntimeIdentifier=win10-x64 /p:TargetFramework=netcoreapp2.1 /p:Configuration=Release /p:ArchiveDir=${BinFolder}/downloads
+dotnet pack src/Microsoft.Atlas.CommandLine.Chocolatey/Microsoft.Atlas.CommandLine.Chocolatey.csproj -c Release -o ${BinFolder}/chocolatey
